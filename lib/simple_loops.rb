28d9da08c957bf48ab9bd9f24b046c7ba5469038
Hannah Kofkin <hkofkin@gmail.com>
2020-04-22 18:36:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,6 +24,10 @@ def output_array(array)
   end
 end
 
-def return_string_array
-  
+def return_string_array(array)
+  count = 0 
+  while count <= array.length do
+    return array.to_s
+    count += 1
+  end
 end 
